75af90c319abd9946640ff8fec31128e1aaaa347
madison <zhuangsen@live.com>
2018-09-28 22:15:24 +0800

    重构

--- overview/src/main/java/com/zs/diveinspringboot/DiveInSpringBootApplication.java
+++ overview/src/main/java/com/zs/diveinspringboot/DiveInSpringBootApplication.java
@@ -1,10 +1,10 @@
-package diveinspringboot;
+package com.zs.diveinspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@ServletComponentScan(basePackages = "com.zs.diveinspringboot.web.servlet")
+//@ServletComponentScan(basePackages = "com.zs.com.zs.diveinspringboot.web.servlet")
 public class DiveInSpringBootApplication {
 
     public static void main(String[] args) {
