d12561182d0f674e12cbda0af51cc31e0fb2def0
madison <zhuangsen@live.com>
2018-09-29 07:46:06 +0800

    springappliation

--- overview/src/main/java/com/zs/diveinspringboot/DiveInSpringBootApplication.java
+++ overview/src/main/java/com/zs/diveinspringboot/DiveInSpringBootApplication.java
@@ -1,17 +1,19 @@
 package com.zs.diveinspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 //@ServletComponentScan(basePackages = "com.zs.com.zs.diveinspringboot.web.servlet")
 public class DiveInSpringBootApplication {
 
     public static void main(String[] args) {
-//	    new SpringApplicationBuilder(DiveInSpringBootApplication.class)
-//                .web(WebApplicationType.NONE)
-//                .properties("abc=sds")
-//                .run(args);
-        SpringApplication.run(DiveInSpringBootApplication.class, args);
+	    new SpringApplicationBuilder(DiveInSpringBootApplication.class)
+                .web(WebApplicationType.NONE)
+                .properties("abc=sds")
+                .run(args);
+//        SpringApplication.run(DiveInSpringBootApplication.class, args);
     }
 }
---
+++ spring-application/src/main/java/com/zs/diveinspringboot/SpringApplicationBootstrap.java
@@ -0,0 +1,38 @@
+package com.zs.diveinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * {@link SpringApplication}
+ */
+//@SpringBootApplication
+public class SpringApplicationBootstrap {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(ApplicationConfiguration.class, args);
+
+        Set<String> sources = new HashSet<>();
+        //配置Class 名称
+        sources.add(ApplicationConfiguration.class.getName());
+//      SpringApplication springApplication =  new SpringApplication(ApplicationConfiguration.class)
+        SpringApplication springApplication = new SpringApplication();
+//        springApplication.setSources(Collections.singleton(ApplicationConfiguration.class.getName()));
+        springApplication.setSources(sources);
+        springApplication.setWebApplicationType(WebApplicationType.NONE);
+        ConfigurableApplicationContext context = springApplication.run(args);
+        System.out.println("Bean:"+context.getBean(ApplicationConfiguration.class));
+    }
+
+    @SpringBootApplication
+    public static class ApplicationConfiguration{
+
+    }
+}
